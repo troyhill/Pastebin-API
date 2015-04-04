f7486481ec88b8efbeda8a639fdd4a9dbb100dfd
Troy Hill <hill.troy@gmail.com>
2015-04-04 18:37:18 -0400

    Update pbPost.R

--- R/pbPost.R
+++ R/pbPost.R
@@ -4,10 +4,11 @@
 # posts as guest by default - otherwise, api_user_password needs to be defined before using code
 
 pbPost <- function(user_key = "", api_dev_key = "", 
-                   text = "hello world!", title = "Untitled", expire = "1W"){
+                   text = "hello world!", title = "Untitled", expire = "1W", privacy_code = "0"){
   expiry <- c("N", "10M", "1H", "1D", "1W", "2W", "1M")
   if(!expire %in% expiry) stop("Expiration time not valid - check pastebin.com/api")
   if(user_key %in% "") password <- ""
+  if(!privacy_code %in% c("0", "1", "2")) stop("`privacy_code` must be `0`, `1`, or `2`.")
 
   pasteList <- NA
   for(i in 1:length(text)){
@@ -15,7 +16,7 @@ pbPost <- function(user_key = "", api_dev_key = "",
                      api_option = "paste",
                      api_dev_key = api_dev_key,
                      api_paste_code = text[i],
-                     api_paste_private = api_paste_private, 
+                     api_paste_private = privacy_code, # 0=public 1=unlisted 2=private 
                      api_paste_name = title, 
                      api_paste_expire_date = expire,
                      api_user_key = user_key
