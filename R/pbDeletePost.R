ccd6eed4aa3abc5941b1f710ef46e44d1fc52b9d
troyhill <hill.troy@gmail.com>
2015-04-04 19:02:26 -0400

    update

--- R/pbDeletePost.R
+++ R/pbDeletePost.R
@@ -1,12 +1,15 @@
 # delete specified post(s)
 # 'paste' is a list of posts to delete (only the part of the url following 'pastebin.com/')
 
-pbDeletePost <- function(paste, user_key = "", api_dev_key = ""){
+pbDeletePost <- function(paste, session_key, api_dev_key){
+  if(session_key %in% "") stop("`session_key` is a required input. Generate one with ?getUserKey")
+  if(api_dev_key %in% "") stop("Your API development key is missing. Get your key by visiting pastebin.com/api")
+  
   for(i in 1:length(paste)){
     postList <- postForm(
       uri = 'http://pastebin.com/api/api_post.php',
       api_dev_key = api_dev_key,
-      api_user_key = user_key, # session key
+      api_user_key = session_key,
       api_paste_key = paste[i],
       api_option = "delete"
     )
--- R/pbPost.R
+++ R/pbPost.R
@@ -3,11 +3,11 @@
 # posts are public by default ('privacy_code')
 # posts as guest by default - to post as a user, a session key ('user_key') is required
 
-pbPost <- function(user_key = "", api_dev_key = "", 
+pbPost <- function(session_key = "", api_dev_key = "", 
                    text = "hello world!", title = "Untitled", expire = "1W", privacy_code = "0"){
   expiry <- c("N", "10M", "1H", "1D", "1W", "2W", "1M")
   if(!expire %in% expiry) stop("Expiration time not valid - check pastebin.com/api")
-  if(user_key %in% "") password <- ""
+  if(session_key %in% "") password <- ""
   if(!privacy_code %in% c("0", "1", "2")) stop("`privacy_code` must be `0`, `1`, or `2`.")
 
   pasteList <- NA
@@ -19,7 +19,7 @@ pbPost <- function(user_key = "", api_dev_key = "",
                      api_paste_private = privacy_code, # 0=public 1=unlisted 2=private 
                      api_paste_name = title, 
                      api_paste_expire_date = expire,
-                     api_user_key = user_key
+                     api_user_key = session_key
     )
     pasteList <- c(pasteList, post[1])
   }
